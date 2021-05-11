c13f1b84f4b5fcde44c301de2ebb09781406c5de
nwdelafu <nick.delafuente1@gmail.com>
2021-05-11 11:02:06 -0700

    Examples from module 2

---
+++ module2/postgresql_example.py
@@ -0,0 +1,42 @@
+""""A basic PostGreSQL workflow"""
+import psycopg2 as pg2
+from query import SQL_INSERT_DATA, SQL_CREATE_TABLE, SQL_SHOW_TABLE
+
+DBNAME = "nthytbho"
+USER = "nthytbho"
+PASSWORD = "omt-EaxOcP5k7QUK0hTvXbTalVaeFNdn"
+HOST = "queenie.db.elephantsql.com"
+
+
+def create_connection(dbname, user, password, host):
+    pg_conn = pg2.connect(dbname=dbname, user=user,
+                          password=password, host=host)
+    return pg_conn
+
+
+def execute_query(conn, query, read=True):
+    pg_curs = conn.cursor()
+    pg_curs.execute(query)
+    if read:
+        # Will fetch data if we are reading so we dont get error
+        results = pg_curs.fetchall()
+        pg_curs.close()
+        return results
+    else:
+        # Will commit our changes if we are Creating, Updating, Deleting
+        conn.commit()
+        pg_curs.close()
+        return "CUD Query Executed"
+
+
+def show_test_table(conn):
+    pg_curs = conn.cursor()
+    pg_curs.execute(SQL_SHOW_TABLE)
+    return pg_curs.fetchall()
+
+
+if __name__ == "__main__":
+    pg_conn = create_connection(DBNAME, USER, PASSWORD, HOST)
+    execute_query(conn=pg_conn, query=SQL_CREATE_TABLE, read=False)
+    execute_query(conn=pg_conn, query=SQL_INSERT_DATA, read=False)
+    print(show_test_table(pg_conn))
---
+++ module2/sql_pipeline_example.py
@@ -0,0 +1,49 @@
+"""SQLite to PostGreSQL pipeline"""
+import sqlite3
+import psycopg2 as pg2
+from query import EXTRACT_CHARACTERS, CREATE_characterscreator_character, INSERT_INTO_charactercreator_character
+
+
+DBNAME = "nthytbho"
+USER = "nthytbho"
+PASSWORD = "omt-EaxOcP5k7QUK0hTvXbTalVaeFNdn"
+HOST = "queenie.db.elephantsql.com"
+
+
+# We want to move charactercreator_character from sqlite -> postgresql
+def create_connections(dbname, user, password, host, sqlite_db="../data/rpg_db.sqlite3"):
+    """Creates a connection to sqlite and postgresql"""
+    sl_conn = sqlite3.connect(sqlite_db)
+    pg_conn = pg2.connect(dbname=dbname, user=user,
+                          password=password, host=host)
+    return sl_conn, pg_conn
+
+
+def execute_query(conn, query, read=True):
+    """Executes queries depending on conn object passed in"""
+    curs = conn.cursor()
+    curs.execute(query)
+    if read:
+        # Will fetch data if we are reading so we dont get error
+        results = curs.fetchall()
+        curs.close()
+        return results
+    else:
+        # Will commit our changes if we are Creating, Updating, Deleting
+        conn.commit()
+        curs.close()
+        return "CUD Query Executed"
+
+
+if __name__ == "__main__":
+    sl_conn, pg_conn = create_connections(DBNAME, USER, PASSWORD, HOST)
+    character_list = execute_query(sl_conn, EXTRACT_CHARACTERS)
+    # character_list = [(1, 'Aliquid iste optio reiciendi', 0, 0, 10, 1, 1, 1, 1), (...), (...), ...]
+    execute_query(
+        conn=pg_conn, query=CREATE_characterscreator_character, read=False)
+
+    pg_curs = pg_conn.cursor()
+    for character in character_list:
+        pg_curs.execute(INSERT_INTO_charactercreator_character, character)
+        pg_conn.commit()
+        pg_curs.close()
