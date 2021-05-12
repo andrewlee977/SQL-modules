1aac6b18352e703f392f3c1453dce6f50464c0dc
Andrew Lee <andrew.lee97@gmail.com>
2021-05-12 08:36:49 -0700

    Add comments

--- module2/insert_titanic.py
+++ module2/insert_titanic.py
@@ -1,4 +1,4 @@
-"""Uploads Titanic csv data """
+"""Uploads Titanic csv data to PostgreSQL"""
 
 import pandas as pd
 import psycopg2 as pg2
@@ -9,29 +9,28 @@ USER = "wnphosbi"
 PASSWORD = "NlMaJqzK0DRgjdU7aNdroiaEmXONFPOU"
 HOST = "queenie.db.elephantsql.com"
 
-# df = pd.read_csv('../data/titanic.csv')
-# curs.copy_from(df, temp_unicommerce_status, sep=',')
-# f.close()age INT NOT NULL,
 
 def csv_to_db(conn):
+    """Extracts, transforms, then loads titanic csv into PostgreSQL"""
     curs = conn.cursor()
     f = open('../data/titanic.csv', 'r')
     next(f)
-    curs.copy_from(f, 'titanic', sep=',', columns=('Survived', 'Pclass', 'Name', 'Sex', 'Age',
-                                                   '\"Siblings/Spouses Aboard\"', '\"Parents/Children Aboard\"', 'Fare'))
+    curs.copy_from(f, 'titanic', sep=',', columns=('Survived', 'Pclass', 'Name',
+                                                   'Sex', 'Age', '\"Siblings/Spouses Aboard\"',
+                                                   '\"Parents/Children Aboard\"', 'Fare'))
     f.close()
+    conn.commit()
     curs.close()
 
 
 def connect_to_db(dbname, user, password, host):
+    """Creates connection to PostgreSQL"""
     conn = pg2.connect(dbname=dbname, user=user, password=password, host=host)
-    # df.to_sql(name='titanic', con=conn, if_exists='replace')
     return conn
-
-# df.to_sql(name='titanic', con=conn, if_exists='replace')
 
 
 def execute_query(conn, query, read=True):
+    """Executes query depending on function (Read or CUD)"""
     curs = conn.cursor()
     curs.execute(query)
     if read:
@@ -58,22 +57,23 @@ SQL_TITANIC_CREATE_TABLE = """
     );
 """
 
+
 SQL_TITANIC_SHOW_TABLE = """
     SELECT * FROM titanic
 """
 
+
 def show_test_table(conn):
+    """Function returns titanic table"""
     curs = conn.cursor()
     curs.execute(SQL_TITANIC_SHOW_TABLE)
     return curs.fetchall()
 
 
-
 if __name__ == "__main__":
+    """Runs functions when file is called"""
     conn = connect_to_db(DBNAME, USER, PASSWORD, HOST)
     execute_query(conn=conn, query=SQL_TITANIC_CREATE_TABLE, read=False)
     csv_to_db(conn)
     results = show_test_table(conn)
     print(results)
-
-
